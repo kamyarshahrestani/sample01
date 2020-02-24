04589111ce318594d17dcb5ce2d95841db48af6b
kamyarshahrestani <seyedkamyar@shahrestani.me>
2020-02-24 23:03:42 +0100

    add cors feature to service

--- CustomerSampleAPI/Startup.cs
+++ CustomerSampleAPI/Startup.cs
@@ -13,11 +13,21 @@ namespace CustomerSampleAPI
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200");
+                });
+            });
             services.AddControllersWithViews();
         }
 
@@ -34,6 +44,8 @@ namespace CustomerSampleAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseHttpsRedirection();
             app.UseDefaultFiles();
